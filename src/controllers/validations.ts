f1deb2859f32d2ea4110cfb28c356abd20300085
Rosmari <rosmarifernandez@hotmail.com>
2021-08-09 15:18:45 -0300

    validaciones para asignar cupon

---
+++ src/controllers/validations.ts
@@ -0,0 +1,25 @@
+export function validationCoupon(codestring:string){
+    const ruleCode = /^[a-zA-Z0-9]{8}/;
+    const testCode = ruleCode.test(codestring) && codestring.length == 8; 
+    return testCode
+}
+
+export function validationDate(dateString:string){
+    const ruleDate = /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/;
+    const testDate = ruleDate.test(dateString);
+    return testDate
+}
+
+export function getErrors(code:string, expiredAt: string, assignedAt:string){ 
+        let errors = {}
+        if(!validationDate(expiredAt))
+             errors = {...errors, dateAssignedAt: 'invalid format'}
+
+        if(!validationDate(assignedAt))
+            errors = {...errors, dateExpiresAt: 'invalid format'}
+
+        if(!validationCoupon(code))
+            errors = {...errors, dateCode: 'invalid format: must contain 8 characters and be alphanumeric'}
+        return errors
+
+}
