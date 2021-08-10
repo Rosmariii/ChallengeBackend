1901ae5d524cf3bec57fb9b3ce2411440d2382b7
Rosmari <rosmarifernandez@hotmail.com>
2021-08-09 22:21:47 -0300

    se creo las rutas get, post, delete de store

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getcoupons, newCoupon, assignCoupon, deleteCoupon } from "../controllers/index.controllers";
+import { getcoupons, newCoupon, assignCoupon, deleteCoupon } from "../controllers/index.coupons";
+import { getStore, newStore, deleteStore } from "../controllers/index.stores";
 const router = Router()
 
 router.get('/coupons', getcoupons)
 router.post('/coupons', newCoupon)
 router.patch('/coupons', assignCoupon)
 router.delete('/coupons/:id', deleteCoupon)
+
+router.get('/store', getStore)
+router.post('/store', newStore)
+router.delete('/store/:id', deleteStore)
+
 
 export default router
