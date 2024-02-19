9a4fc755aa20d2d44bedb6856c42220895579c53
Pola <jditoma@payana.la>
2024-02-19 17:10:30 -0300

    algunas mejoras

--- src/controllers/index.coupons.ts
+++ src/controllers/index.coupons.ts
@@ -7,16 +7,22 @@ import * as EmailValidator from 'email-validator';
 
 export const getcoupons = async (req: Request, res: Response): Promise<Response> => {
     try {
-        const email = (req.query.email);
-        const coupon = (req.query.coupon);
+        // Acá podemos aprovechar la destructuracion de objetos de JS para tener un código más limpio.
+        const { email, coupon } = req.query;
+
         const response: QueryResult = await pool.query('SELECT * FROM coupons WHERE customer_email = $1 and code = $2', [email, coupon]);
-        if (response.rowCount == 0){ 
+
+        // no hace falta el === 0, ya que si es 0 es falsy y si es mayor a 0 es truthy. También ojo con usar sólo ==, siempre ===
+        if (response.rowCount){ 
             return res.status(404).json({message: 'Not found'})
-        } else {
-            return res.status(200).json(response.rows)
-        }     
+        } 
+        
+        // no hace falta el else, ya que si el if de arriba es true, el código termina en el return y no sigue ejecutando
+        return res.status(200).json(response.rows)
     }
     catch (e) {
+        // siempre loguea el error para entender mejor.
+        console.log(e)
         return res.status(500).json('internal error');
     }
 }
@@ -25,42 +31,47 @@ export const newCoupon = async (req: Request, res: Response): Promise<Response> 
     
     const {assignedAt, code, expiresAt} = req.body
 
-    if(validationCoupon(code) && validationDate(assignedAt) && validationDate(expiresAt)) {
-        const response: QueryResult = await pool.query('INSERT INTO coupons (assigned_at, code, expires_at) VALUES ($1, $2, $3)', [assignedAt, code, expiresAt]);
-        return res.status(201).json({messege:'coupon saved',body: {code, assignedAt, expiresAt,}})
-    } else {
+    // Lo paso a una variable que explica lo que esta validando para que el if sea más legible
+    const validateInputs = validationCoupon(code) && validationDate(assignedAt) && validationDate(expiresAt)
+    if(!validateInputs) {
+        // puse el 422 acá. Si algo no esta bien, respondo. Si tenemos más lógica de negocio más adelante, va por afuera del if.
         return res.status(422).json(getErrors(code,expiresAt,assignedAt));
     }
+    
+    const response: QueryResult = await pool.query('INSERT INTO coupons (assigned_at, code, expires_at) VALUES ($1, $2, $3)', [assignedAt, code, expiresAt]);
+    res.status(201).json({messege:'coupon saved',body: {code, assignedAt, expiresAt,}})
 }
 
 export const assignCoupon = async (req: Request, res: Response): Promise<Response> => {
     
-    const {email, code} = (req.body);
+    const { email, code } = req.body;
     const chekemail = EmailValidator.validate(email);
     
-    if (chekemail) {  
-        const resEmail: QueryResult = await pool.query('SELECT * FROM coupons WHERE customer_email = $1', [email]);
-        if(resEmail.rowCount == 0) {
-            const assignCoupon: QueryResult = await pool.query('UPDATE coupons SET customer_email = $1 WHERE code = $2', [email, code]);
-            return res.status(201).json({message: 'saved correctly', body: {email,code} })
-        } else {
-            return res.status(406).json({message: 'Error: The email was already assigned to a coupon or the coupon does not exist '})
-            }           
-    } else {
+    // evitamos tantos ifs anidados, nunca son lindos de leer.
+    if (chekemail) {
         return res.status(422).json({message: 'the email format is not correct'})
-    }
+    }  
+
+    const resEmail: QueryResult = await pool.query('SELECT * FROM coupons WHERE customer_email = $1', [email]);
+    if(!resEmail.rowCount) {
+        return res.status(406).json({message: 'Error: The email was already assigned to a coupon or the coupon does not exist '})   
+    } 
+
+    const assignCoupon: QueryResult = await pool.query('UPDATE coupons SET customer_email = $1 WHERE code = $2', [email, code]);
+    return res.status(201).json({message: 'saved correctly', body: {email,code} })
 }
 
 export const deleteCoupon = async (req: Request, res: Response): Promise<Response> => {
     try { 
         const id = parseInt(req.params.id);
+
         const result = await pool.query('SELECT * FROM coupons WHERE id = $1 and code is not null and customer_email is null', [id]);
-        if (result.rowCount == 1) {
-            await pool.query('DELETE FROM coupons WHERE id = $1', [id]);
-            return res.status(200).json({message: 'deleting'})
-        } else {
+        if (!result.rowCount) {
             return res.status(404).json({ message: 'the code is not valid or does not exist'})
-        }
+        } 
+
+        await pool.query('DELETE FROM coupons WHERE id = $1', [id]);
+        return res.status(200).json({message: 'deleting'})
     }
     catch(e) {
         return res.send(e)
--- src/controllers/index.stores.ts
+++ src/controllers/index.stores.ts
@@ -4,21 +4,24 @@ import { QueryResult } from "pg";
 
 export const getStore = async (req: Request, res: Response): Promise<Response> => {
     let result = []
-    let response: QueryResult = await pool.query('SELECT * FROM stores');
+    let response: QueryResult = await pool.query('SELECT * FROM stores limit 100'); // por las dudas, siempre pongamos un límite, el día que sean 1000000 de registros, no va a andar.
     let count = response.rowCount
+    const name = req.query.hasOwnProperty('name')
+    const pageInput = req.query.hasOwnProperty('page')
 
-    if(req.query.hasOwnProperty('name')) {
-        const name = req.query.name;
-        let response: QueryResult = await pool.query('SELECT * FROM stores WHERE name = $1', [name]);
-        result = response.rows
-    } else {
-        result = response.rows
-    }
+    // toda esta lógica es incorrecta acá. Obtenes todos los resultados y después los cortas. Mejor pedirle a la base de datos que te devuelva sólo los que necesitas. y evitar todo el cómputo este
+    // te lo va a agradecer la base de datos, el servidor y el cliente (que va a esperar menos)
+    // quedaría algo así: (ojo, no lo pensé mucho, pero es para que entiendas la idea)
+    const start = pageInput * 10
+    const end = start+10
+    const result = await pool.query('SELECT * FROM stores WHERE name = $1 limit $2 offset $3', [name, end, start]);
 
-    if(req.query.hasOwnProperty('page')) {
-        const page = Number(req.query.page);
+    
+    // todo esto no va y lo del name y eso debería incluirse en la query de arriba.
+    if(pageInput) {
+        const page = Number(pageInput);
         
-        if(page > 0) {
+        if(page) {
             const start = page * 10
             const end = start+10
             result = result.slice(start, end)
@@ -26,6 +29,7 @@ export const getStore = async (req: Request, res: Response): Promise<Response> =
     } else {
         result = result.slice(0, 10)
     }
+
     
     return res.json({result, total_store: count})
 }
@@ -33,12 +37,13 @@ export const getStore = async (req: Request, res: Response): Promise<Response> =
 export const newStore = async (req: Request, res: Response): Promise<Response> => {
     const {name, address} = req.body;
 
-    if(name !== null && name.length > 0 && address !== null && address.length > 0) {
-        await pool.query('INSERT INTO stores (name, address) VALUES ($1, $2)', [name, address]);
-        return res.status(200).json({message:'has been created successfully', body: { name, address}})
-    } else {
+    const validationInput = name !== null && name.length > 0 && address !== null && address.length > 0
+    if(!validationInput) {
         return res.status(422).json({message: 'the name and address field cannot be null'})
-    }
+    } 
+
+    await pool.query('INSERT INTO stores (name, address) VALUES ($1, $2)', [name, address]);
+    return res.status(200).json({message:'has been created successfully', body: { name, address}})
 }
 
 export const deleteStore = async (req: Request, res: Response): Promise<Response> => {
@@ -47,7 +52,7 @@ export const deleteStore = async (req: Request, res: Response): Promise<Response
         const id = parseInt(req.params.id);
         const result = await pool.query('SELECT * FROM stores WHERE id = $1', [id]);
 
-        if (result.rowCount == 1) { 
+        if (result.rowCount) { 
             await pool.query('DELETE FROM stores WHERE id = $1', [id]);
             return res.status(200).json({message: 'deleting'})
         } else {
